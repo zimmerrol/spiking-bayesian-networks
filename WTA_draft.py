08e5640f5a717261ea5875b9f53863a7e6a769e7
Paul@Alublech <sitzkringel@web.de>
2019-02-18 15:31:07 +0100

    fixed spike trains and added test case

--- WTA_draft.py
+++ WTA_draft.py
@@ -33,37 +33,65 @@ X_freq = 20 + X*70
 # In[20]:
 
 
-def generate_spike_trains(X_freq, T, T_image = 0.250, delta_T = 0.01):
+def generate_spike_trains(X_freq, T, T_image = 0.250, delta_T = 0.0001):
+    """
+    Parameters
+        ----------
+        X_freq : ~numpy.ndarray
+            Image sequence
+
+        T : float
+            total time length of the spiek train seconds
+
+        T_image : float
+            duration to show each image in seconds
+
+        delta_T : float
+            sampling freq discretization of the poisson process
+    """
     n_samples = int(np.ceil(T / T_image))
     sample_steps = int(np.ceil(T_image/delta_T))
-    
+
     n_steps = n_samples * sample_steps
-    
-    # generate rates
+
+    # generate time dependant image showing rates
     rates = np.zeros((n_steps, X_freq.shape[1]))
     sample_indices = np.random.randint(0, len(X_freq), size=n_samples)
-    rates = np.stack(X_freq[sample_indices])
-    
+    for i, sample_index in enumerate(sample_indices):
+        rates[i*sample_steps:(i+1)*sample_steps] = X_freq[sample_index]
+
     # now generate the spike trains
     p = np.random.uniform(0.0, 1.0, size=rates.shape)
     y = (rates*delta_T > p).astype(np.float32)
-        
+
     return y
 
 
 # In[21]:
 
 
-X_spikes = generate_spike_trains(X_freq, 1000, delta_T=1e-2)
+# X_spikes = generate_spike_trains(X_freq, 1000, delta_T=1e-2)
+test = np.ones(shape=(100))*10.0
+test = np.vstack((test, np.ones(shape=(100))*20.0) )
+test = test.T
+X_spikes = generate_spike_trains(test, 1000, delta_T=1e-2)
+
 
 
 # In[19]:
 
 
-a = 50
-for i in range(28*28):
-    plt.scatter(np.arange(a)[X_spikes[:a, i] == 1.0], i*np.ones(np.sum(X_spikes[:a, i] == 1.0)), s=0.5, c='C1')
-plt.show()
+a = 500
+# number of points to print
+
+# neurons_to_plot = range(28*28)[0:-1:20]
+neurons_to_plot = [0,1]
+for i in neurons_to_plot:
+    plt.scatter(np.arange(a)[X_spikes[:a, i] == 1.0],
+        i*np.ones(np.sum(X_spikes[:a, i] == 1.0)),
+        s=0.5, c='C1')
+
+plt.show(block=True)
 
 
 # In[236]:
@@ -88,31 +116,31 @@ class Network:
         self._m_k = m_k
         self._eta_v = eta_v
         self._eta_b = eta_b
-        
+
         self._V = np.random.normal(scale=1e-3, size=(self._n_outputs, self._n_inputs))
         self._b = np.zeros((self._n_outputs, 1))
-                
+
     def step(self, inputs):
         inputs = inputs.reshape((-1, 1))
         assert len(inputs) == self._n_inputs, "Input length does not match"
-        
+
         u = np.dot(self._V, inputs) + self._b
-        
+
         z = np.zeros((self._n_outputs, 1))
-        
+
         p_z = np.exp(u) / np.sum(np.exp(u)) * self._delta_t * self._r_net
-        
+
         sum_p_z = np.cumsum(p_z)
         diff = sum_p_z - np.random.uniform(0, 1, 1) > 0
-       
+
         k = np.argmax(diff)
-        
+
         if diff[k]:
             z[k] = 1.0
-        
+
         self._b += self._delta_t * self._eta_b * (self._r_net * self._m_k - dirac(z - 1))
         self._V += self._delta_t * self._eta_v * dirac(z - 1) * (inputs.T - sigmoid(self._V))
-        
+
         return z
 
 
