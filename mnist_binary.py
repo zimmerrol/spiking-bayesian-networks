5921f410153e634316bad1de43fa00a9fa2c8266
FlashTek <rzrolandzimmermann@gmail.com>
2019-02-20 17:22:16 +0100

    clean event-based WTA model implemented

--- mnist_binary.py
+++ mnist_binary.py
@@ -1,12 +1,9 @@
 import numpy as np
-import matplotlib
-matplotlib.use("Qt5Agg")
-print("BACKEND: ", matplotlib.get_backend())
-from matplotlib import pyplot as plt
 import utility as ut
 import network as nt
 from tqdm import tqdm as tqdm
-import plot as pt
+from plot import WeightPCAPlotter, WeightPlotter, CurvePlotter
+from collections import deque
 
 delta_T = 1e-3
 
@@ -33,45 +30,67 @@ X = X[:, (28-H)//2:-(28-H)//2, (28-W)//2:-(28-W)//2]
 Y = y_test[selection]
 X = X.reshape((len(X), -1)) / 255.0
 X = (X > 0.5).astype(np.float32)
+X_frequencies = X * 70.0 + 20.0
 
-if spiking_input:
-    X_frequencies = X * 70.0 + 20.0
-    X_spikes = ut.generate_spike_trains(X_frequencies, 1000, delta_T=delta_T)
-else:
-    X_spikes = ut.generate_constant_trains(X, 1000, delta_T=delta_T)
 
-net = nt.BinaryWTANetwork(n_inputs=n_inputs, n_outputs=n_outputs,
-                            delta_T=delta_T, r_net=r_net, m_k=m_k, eta_v=1e+1, eta_b=1e+3)
-    
+def data_generator(time):
+    image_index = np.random.uniform(0, len(X), 1).astype(np.int32)
+
+    if spiking_input:
+        rate = X_frequencies[image_index]
+        y = (np.random.uniform(0, 1, n_inputs) < delta_T * rate).astype(np.uint8)
+    else:
+        y = X[image_index]
+
+    return y
+
+
+net = nt.EventBasedBinaryWTANetwork(n_inputs=n_inputs, n_outputs=n_outputs,
+                            r_net=r_net, m_k=m_k, eta_v=1e-2, eta_b=1e+0, max_trace_length=1000,
+                            delta_T=1e-3, tau=5e-2)
+
 
 # train
-from plot import WeightPCAPlotter, WeightPlotter
+
 pca_plotter = WeightPCAPlotter(X, Y, n_outputs, labels)
 weights_plotter = WeightPlotter(ut.sigmoid(net._V).reshape((-1, W, H)))
-
-from collections import deque
+likelihood_plotter = CurvePlotter(x_label="Time [s]", y_label="$log(p(y))$")
 
 average_length_likelihood = 500
 
-pbar = tqdm(enumerate(X_spikes))
-for batch_index, sample_batch in pbar:
-    # update figure here
-    log_likelihoods = deque([])
-    for sample in sample_batch:
-        net.step(sample)
+likelihoods = []
 
-        # log likelihood
-        Ak = np.sum(np.log(1+np.exp(net._V)), -1)
+log_likelihoods = deque([])
 
-        pi = ut.sigmoid(net._V)
-        log_likelihoods.append(np.log(1.0/n_outputs) + np.log(np.sum(np.prod(sample * pi + (1-sample) * (1-pi), axis=-1))))
+T_max = 100
 
-        if len(log_likelihoods) > average_length_likelihood:
-            log_likelihoods.pop()
+pbar = tqdm(total=T_max, unit='Time [s]')
+while net._current_time < T_max:
+    net.step(data_generator)
 
-    weights = ut.sigmoid(net._V)
+    pbar.n = int(net._current_time * 1000) / 1000
+    pbar.update(0)
 
-    pca_plotter.update(weights)
-    weights_plotter.update(weights)
+    # log likelihood
 
-    pbar.set_description(f'<sigma(V)> = {np.mean(weights):.4f}, <b> = {np.mean(net._b):.4f}, <L(y)> = {np.mean(log_likelihoods)}')
+    sample = net._trace[-1][1].reshape((1, -1))
+    Ak = np.sum(np.log(1+np.exp(net._V)), -1)
+
+    pi = ut.sigmoid(net._V)
+    log_likelihoods.append(np.log(1.0/n_outputs) + np.log(np.sum(np.prod(sample * pi + (1-sample) * (1-pi), axis=-1))))
+
+    if len(log_likelihoods) > average_length_likelihood:
+        log_likelihoods.popleft()
+
+    # update plots
+    if int(pbar.n) > len(likelihoods):
+        likelihoods.append(np.mean(log_likelihoods))
+
+        weights_plotter.update(ut.sigmoid(net._V))
+        pca_plotter.update(ut.sigmoid(net._V))
+        likelihood_plotter.update(likelihoods)
+
+    pbar.set_description(
+        f'<sigma(V)> = {np.mean(ut.sigmoid(net._V)):.4f}, <b> = {np.mean(net._b):.4f}, <L(y)> = {np.mean(log_likelihoods):.4f}')
+
+pbar.close()
--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import utility as ut
 from abc import ABC, abstractclassmethod
+from collections import deque
 
 # ------------------------------------------------------------------ #
 # network
@@ -72,6 +73,265 @@ class BinaryWTANetwork():
 
         self._b += self._delta_T * self._eta_b * (self._delta_T * self._r_net * self._m_k - ut.dirac(z - 1))
         self._V += self._delta_T * self._eta_v * ut.dirac(z - 1) * (inputs.T - ut.sigmoid(self._V))
+
+        return z
+
+
+class EventBasedBinaryWTANetwork():
+    """
+    Implementation of the (soft) Winner Takes All (WTA) network, assuming Bernoulli distributions
+    """
+
+    def __init__(self, n_inputs, n_outputs, r_net, m_k, eta_v, eta_b, max_trace_length):
+        """
+            Parameters
+            ----------
+            r_net : float
+                global target firing rate of the whole network
+
+            m_k :  numpy.ndarray
+                size n_outputs
+                has to sum up to one, propto relative target firing rate of
+                neuron k
+
+            eta_v : float
+                learning rate of v eq (8)
+
+            eta_b : float
+                learing rate of b eq. (7)
+        """
+
+        self._n_inputs = n_inputs
+        self._n_outputs = n_outputs
+        self._r_net = r_net
+        self._m_k = m_k
+        self._eta_v = eta_v
+        self._eta_b = eta_b
+
+        if np.isscalar(self._m_k):
+            self._m_k = np.ones((n_outputs, 1)) * self._m_k
+
+        assert len(self._m_k) == n_outputs, "Length of m_ks does not match number of output neurons"
+
+        self._V = np.random.normal(scale=1e-3, size=(self._n_outputs, self._n_inputs))
+        self._b = np.log(self._m_k * self._r_net)
+
+        self._current_time = 0.0
+
+        self._trace = deque([])
+        self._max_trace_length = max_trace_length
+
+    def step(self, data_generator_fn):
+        # sample  isi 
+        isi = - np.log(np.random.uniform()) / self._r_net
+
+        inputs = data_generator_fn(self._current_time + isi)
+        inputs = inputs.reshape((-1, 1))
+        assert len(inputs) == self._n_inputs, "Input length does not match"
+
+        # u = V * input + b
+        u = np.dot(self._V, inputs) + self._b
+
+        z = np.zeros((self._n_outputs, 1))
+
+        # p = softmax(u)
+        p_z = np.exp(u) / np.sum(np.exp(u) + 1e-8)
+
+        # sample from softmax distribution
+        sum_p_z = np.cumsum(p_z)
+        diff = sum_p_z - np.random.uniform(0, 1, 1) > 0
+        k = np.argmax(diff)
+
+        z[k] = 1.0
+
+        self._b += self._eta_b * (isi * self._r_net * self._m_k - ut.dirac(z - 1))
+        self._V += self._eta_v * ut.dirac(z - 1) * (inputs.T - ut.sigmoid(self._V))
+
+        self._current_time += isi
+
+        self._trace.append((self._current_time, inputs, z, u, self._V, self._b))
+
+        if len(self._trace) > self._max_trace_length:
+            self._trace.pop()
+
+        return z
+
+
+class EventBasedInputEPSPBinaryWTANetwork():
+    """
+    Implementation of the (soft) Winner Takes All (WTA) network, assuming Bernoulli distributions
+    """
+
+    def __init__(self, n_inputs, n_outputs, r_net, m_k, eta_v, eta_b, max_trace_length, delta_T=1e-3, tau=1e-2):
+        """
+            Parameters
+            ----------
+            r_net : float
+                global target firing rate of the whole network
+
+            m_k :  numpy.ndarray
+                size n_outputs
+                has to sum up to one, propto relative target firing rate of
+                neuron k
+
+            eta_v : float
+                learning rate of v eq (8)
+
+            eta_b : float
+                learing rate of b eq. (7)
+        """
+
+        self._n_inputs = n_inputs
+        self._n_outputs = n_outputs
+        self._r_net = r_net
+        self._m_k = m_k
+        self._eta_v = eta_v
+        self._eta_b = eta_b
+        self._delta_T = delta_T
+        self._tau = tau
+
+        if np.isscalar(self._m_k):
+            self._m_k = np.ones((n_outputs, 1)) * self._m_k
+
+        assert len(self._m_k) == n_outputs, "Length of m_ks does not match number of output neurons"
+
+        self._V = np.random.normal(scale=1e-3, size=(self._n_outputs, self._n_inputs))
+        self._b = np.log(self._m_k * self._r_net)
+
+        self._current_time = 0.0
+
+        self._trace = deque([])
+        self._max_trace_length = max_trace_length
+
+    def step(self, data_generator_fn):
+        # sample  isi
+        isi = - np.log(np.random.uniform()) / self._r_net
+
+        new_time = self._current_time + isi
+
+        # now go back from T + isi - tau to T + isi, calculate input data
+        # calculate the activations
+        # update the weights
+
+        time_start = new_time - self._tau
+
+        total_inputs = np.zeros((self._n_inputs, 1))
+        for time in np.arange(time_start, new_time, self._delta_T):
+            inputs = data_generator_fn(time)
+            inputs = inputs.reshape((-1, 1))
+            inputs *= np.exp(-(new_time - time) / self._tau)
+            assert len(inputs) == self._n_inputs, "Input length does not match"
+            total_inputs += inputs
+
+        # u = V * input + b
+        u = np.dot(self._V, inputs) + self._b
+
+        z = np.zeros((self._n_outputs, 1))
+
+        # p = softmax(u)
+        p_z = np.exp(u) / np.sum(np.exp(u) + 1e-8)
+
+        # sample from softmax distribution
+        sum_p_z = np.cumsum(p_z)
+        diff = sum_p_z - np.random.uniform(0, 1, 1) > 0
+        k = np.argmax(diff)
+
+        z[k] = 1.0
+
+        self._b += self._eta_b * (isi * self._r_net * self._m_k - ut.dirac(z - 1))
+        self._V += self._eta_v * ut.dirac(z - 1) * (inputs.T - ut.sigmoid(self._V))
+
+        self._current_time += isi
+
+        self._trace.append((self._current_time, inputs, z, u, self._V, self._b))
+
+        if len(self._trace) > self._max_trace_length:
+            self._trace.pop()
+
+        return z
+
+
+class EventBasedOutputEPSPBinaryWTANetwork():
+    """
+    Implementation of the (soft) Winner Takes All (WTA) network, assuming Bernoulli distributions
+    """
+
+    def __init__(self, n_inputs, n_outputs, r_net, m_k, eta_v, eta_b, max_trace_length, delta_T=1e-3, tau=1e-2):
+        """
+            Parameters
+            ----------
+            r_net : float
+                global target firing rate of the whole network
+
+            m_k :  numpy.ndarray
+                size n_outputs
+                has to sum up to one, propto relative target firing rate of
+                neuron k
+
+            eta_v : float
+                learning rate of v eq (8)
+
+            eta_b : float
+                learing rate of b eq. (7)
+        """
+
+        self._n_inputs = n_inputs
+        self._n_outputs = n_outputs
+        self._r_net = r_net
+        self._m_k = m_k
+        self._eta_v = eta_v
+        self._eta_b = eta_b
+        self._delta_T = delta_T
+        self._tau = tau
+
+        if np.isscalar(self._m_k):
+            self._m_k = np.ones((n_outputs, 1)) * self._m_k
+
+        assert len(self._m_k) == n_outputs, "Length of m_ks does not match number of output neurons"
+
+        self._V = np.random.normal(scale=1e-3, size=(self._n_outputs, self._n_inputs))
+        self._b = np.log(self._m_k * self._r_net)
+
+        self._current_time = 0.0
+
+        self._trace = deque([])
+        self._max_trace_length = max_trace_length
+
+    def step(self, data_generator_fn):
+        # sample  isi
+        isi = - np.log(np.random.uniform()) / self._r_net
+        new_time = self._current_time + isi
+
+        inputs = data_generator_fn(self._current_time + isi)
+        inputs = inputs.reshape((-1, 1))
+        assert len(inputs) == self._n_inputs, "Input length does not match"
+
+        # u = V * input + b
+        u = np.dot(self._V, inputs) + self._b
+
+        z = np.zeros((self._n_outputs, 1))
+
+        # p = softmax(u)
+        p_z = np.exp(u) / np.sum(np.exp(u) + 1e-8)
+
+        # sample from softmax distribution
+        sum_p_z = np.cumsum(p_z)
+        diff = sum_p_z - np.random.uniform(0, 1, 1) > 0
+        k = np.argmax(diff)
+
+        z[k] = 1.0
+
+        self._b += self._delta_T * self._eta_b * (isi * self._r_net * self._m_k - ut.dirac(z - 1))
+
+        for time in np.arange(new_time, new_time + self._tau, self._delta_T):
+            self._V += self._delta_T * self._eta_v * ut.dirac(z - 1) * (inputs.T - ut.sigmoid(self._V))
+
+        self._current_time += isi
+
+        self._trace.append((self._current_time, inputs, z, u, self._V, self._b))
+
+        if len(self._trace) > self._max_trace_length:
+            self._trace.pop()
 
         return z
 
